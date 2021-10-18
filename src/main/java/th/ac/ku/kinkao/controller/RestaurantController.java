acc1896468921fd35fc15c006b2b46931a8c31a1
mmengspv <supavet.a@ku.th>
2021-10-19 00:32:19 +0700

    feat: add OAuth2 Google and Dockerfile

--- src/main/java/th/ac/ku/kinkao/controller/RestaurantController.java
+++ src/main/java/th/ac/ku/kinkao/controller/RestaurantController.java
@@ -1,5 +1,6 @@
 package th.ac.ku.kinkao.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import th.ac.ku.kinkao.model.Restaurant;
+import th.ac.ku.kinkao.service.RestaurantService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +17,12 @@ import java.util.List;
 @Controller
 @RequestMapping("/restaurant")
 public class RestaurantController {
-    private List<Restaurant> restaurantList = new ArrayList<>();
+    @Autowired
+    private RestaurantService service;
 
     @GetMapping
     public String getRestaurant(Model model){
-        model.addAttribute("restaurants", restaurantList);
+        model.addAttribute("restaurants", service.getAll());
         return "restaurant";
     }
 
@@ -31,7 +34,7 @@ public class RestaurantController {
 
     @PostMapping("/add")
     public String addRestaurant(@ModelAttribute Restaurant restaurant, Model model){
-        restaurantList.add(restaurant);
+        service.addRestaurant(restaurant);
         return "redirect:/restaurant";
     }
 }
